65d49c83a1c2c6993cc5564eaa504d7dbffbf24d
Ryan Castner <castner.rr@gmail.com>
2018-10-24 08:27:00 -0400

    xo lint fixes

--- index.js
+++ index.js
@@ -2,23 +2,25 @@
  * Fisher-Yates shuffles an array of items in O(n) time.
  * The algorithm effectively puts all elements into a hat and continually
  * draws random elements from the hat until no elements remain.
+ * @param {array} items - An array of items.
+ * @returns {array} shuffledItems - A new array of shuffled items.
  */
-export default items => {
+module.exports = items => {
   if (!isArray(items)) {
     throw new TypeError(`Expected Array, got ${typeof items}`);
   }
-  let shuffledItems = [...items];
+  const shuffledItems = [...items];
   for (let index = 0; index < shuffledItems.length - 2; index++) {
-    let randomIndex = Math.floor(Math.random() * index);
-    // swap indices with es6 destructuring e.g. let [a, b] = [b, a]
+    const randomIndex = Math.floor(Math.random() * index);
+    // Swap indices with es6 destructuring e.g. [a, b] = [b, a]
     [shuffledItems[index], shuffledItems[randomIndex]] = [
       shuffledItems[randomIndex],
-      shuffledItems[index]
+      shuffledItems[index],
     ];
   }
   return shuffledItems;
 };
 
 function isArray(x) {
-  return !!x && x.constructor === Array;
+  return Boolean(x) && x.constructor === Array;
 }
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-import test from "ava";
-import shuffle from ".";
+import test from 'ava';
+import shuffle from '.';
 
-test("Shuffle", t => {
+test('Shuffle', t => {
   const err = t.throws(() => {
     shuffle(123);
   }, TypeError);
-  t.is(err.message, "Expected Array, got number");
+  t.is(err.message, 'Expected Array, got number');
 
   t.notDeepEqual(shuffle([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]);
 });
